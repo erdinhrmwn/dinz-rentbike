b529d2102ae6569c3955f60b1469d8a32e529487
Erdin <erdinhrmwn@gmail.com>
2026-06-17 20:13:03 +0700

    feat: add graceful shutdown to echo server

--- internal/bootstrap/server.go
+++ internal/bootstrap/server.go
@@ -1,11 +1,16 @@
 package bootstrap
 
 import (
+	"context"
 	"dinz-rentbike/internal/delivery/http/middleware"
 	"dinz-rentbike/pkg/logger"
 	"dinz-rentbike/pkg/response"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
@@ -16,8 +21,8 @@ func (a *App) Run() {
 	e.HideBanner = true
 	e.HidePort = true
 
-	e.Use(echoMiddleware.RequestID())
 	e.Use(echoMiddleware.Recover())
+	e.Use(echoMiddleware.RequestID())
 	e.Use(middleware.RequestLoggerMiddleware())
 
 	e.GET("/", func(c echo.Context) error {
@@ -25,8 +30,28 @@ func (a *App) Run() {
 	})
 
 	addr := fmt.Sprintf("%s:%d", a.Config.App.Host, a.Config.App.Port)
-	logger.Log.Info().Str("address", addr).Msg("starting server")
-	if err := e.Start(addr); err != nil {
-		logger.Log.Fatal().Err(err).Msg("failed to start server")
+
+	// Start server in goroutine
+	go func() {
+		logger.Log.Info().Str("address", addr).Msg("starting server")
+		if err := e.Start(addr); err != nil && err != http.ErrServerClosed {
+			logger.Log.Fatal().Err(err).Msg("failed to start server")
+		}
+	}()
+
+	// Wait for interrupt signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Log.Info().Msg("shutting down server")
+
+	// Graceful shutdown with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		logger.Log.Fatal().Err(err).Msg("server forced to shutdown")
 	}
+
+	logger.Log.Info().Msg("server stopped gracefully")
 }
